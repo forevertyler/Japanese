68dc1101e5087d9b28d7a186185350c93f49e11d
forevertyler <1318719240@qq.com>
2020-05-24 16:58:08 +0800

    z

---
+++ js/cover.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\'bigCover\'>");
+document.writeln("");
+document.writeln("	<a href=\'jpcn.html\'>根据假名回忆汉语</a>");
+document.writeln("	<a href=\'cnjp.html\'>根据汉语回忆日语</a>");
+document.writeln("	<p>改变主意不想背了</p>");
+document.writeln("</div>");
---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(".remember").on('click', function(e) {
+
+    $(".bigCover").show();
+})
+$(".bigCover p").on('click', function(e) {
+
+    $(".bigCover").hide();
+})
